ecf7ad1aaf74918367f8dc9b774663e3b642f37f
VasylMartin <vasiliymartiniouk@gmail.com>
2022-08-08 18:09:37 +0300

    T1 done

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,23 @@
+const startBtn = document.querySelector('[data-start]')
+const stopBtn = document.querySelector('[data-stop]')
+const body = document.querySelector('body')
+let intervalId = null
+stopBtn.setAttribute('disabled', true)
 
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+startBtn.addEventListener('click', () => {
+intervalId = setInterval(() => {
+        body.style.backgroundColor = getRandomHexColor()
+    }, 1000)
+    startBtn.setAttribute('disabled', true)
+    stopBtn.removeAttribute('disabled', true)
+})
+
+stopBtn.addEventListener('click', () => {
+    clearInterval(intervalId)
+    startBtn.removeAttribute('disabled', true)
+    stopBtn.setAttribute('disabled', true)
+})
